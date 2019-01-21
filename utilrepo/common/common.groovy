44b9572cf35e42a35cab1bbb5c75b065b35e8db5
Jyothi <jyothinayak.123@gmail.com>
2019-01-21 09:09:20 +0000

    update

---
+++ utilrepo/common/common.groovy
@@ -0,0 +1,39 @@
+def uploadWarArtifactory() {
+	script {
+		def server = Artifactory.newServer  url: "${props.ARTIFACTORY_ID}",username:'admin',password:'password'
+               def uploadSpec = """{
+   	
+                "files":[
+                    {
+                   "pattern":"target/*.war",
+			"target": "repo/${artifactId}/${version}.${BUILD_NUMBER}/"
+			}]
+		}"""
+		server.upload(uploadSpec) 	
+	}
+}
+
+def sonar(){
+	def mvncmd=props.SONAR_SCAN
+	def sonarurl=props.SONAR_HOST
+	def url=mvncmd+sonarurl
+	sh "${url}"
+}
+def sendEmail() {
+	emailext( 
+			subject: '${DEFAULT_SUBJECT}', 
+			body: '${DEFAULT_CONTENT}',
+		to: PROPS.RECEPIENT_MAIL_ID
+		);
+	print 'mail sent'
+}
+
+def failureEmail(err) {
+	emailext( 
+			subject: 'BUILD Failure', 
+		         body: "${err}",
+		        to: PROPS.RECEPIENT_MAIL_ID
+		);
+	print 'mail sent'
+}
+return this
---
+++ utilrepo/java/java-ci-cd.groovy
@@ -0,0 +1,54 @@
+
+def function(props) {
+	stage('stageCheckoutProject') {
+		app_url =props.JAVA_APP_REPO_GIT_URL
+		echo "${app_url}"
+		git "${app_url}"
+		
+			
+		pom = readMavenPom file: props.POM_FILE
+		artifactId=pom.artifactId
+		echo "${artifactId}"
+		version=pom.version
+		print 'Checkout Project Success'
+	}
+	
+	stage('sonar'){
+		commonUtility.sonar();
+	}
+	stage('Build') {
+		/*sh props.SONAR_SCAN+' '+props.SONAR_HOST*/
+		sh props.MAVEN_BUILD
+		print 'Build Automation Success'
+    }
+	
+	stage('Artifactory') {
+		commonUtility.uploadWarArtifactory();
+		
+		print 'Build Management Success'
+	}
+	stage('tomcat installation ') {
+	def Install = false;
+	try {
+		input message: 'Install?', ok: 'Install'
+		Install = true
+		} catch (err) {
+	Install = false
+	
+	}
+	
+       if (Install){   
+	 
+	  sh "docker run -d --name tom -p 9001:8080 tomcat"
+        }
+    }	
+	stage('deploy'){
+		sh  props.TOMCAT_DEPLOY+' '+props.TOMCAT_LOCATION
+	}
+	
+	stage('email')
+	{
+		commonUtility.sendEmail();
+	}
+}
+return this
